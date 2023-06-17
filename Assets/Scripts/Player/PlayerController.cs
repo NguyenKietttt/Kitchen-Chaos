50c8f19fcf8d8f9eb22b589af7169556f1c20fb1
Kiet Nguyen <nkiet84@gmail.com>
2023-06-17 15:14:27 +0700

    update new input system

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,11 +5,12 @@ public class PlayerController : MonoBehaviour
     private const int MOVING_SPEED = 7;
     private const int ROTATION_SPEED = 10;
 
+    [SerializeField] private InputManager _inputManager;
     [SerializeField] private PlayerAnimator _playerAnimator;
 
     private void Update()
     {
-        Vector2 input = GetInputVector();
+        Vector2 input = _inputManager.GetInputVectorNormalized();
         bool isMoving = IsMoving(input);
 
         if (isMoving)
@@ -21,42 +22,13 @@ public class PlayerController : MonoBehaviour
         _playerAnimator.UpdateWalkingAnim(isMoving);
     }
 
-    private static bool IsMoving(Vector2 input)
+    private bool IsMoving(Vector2 input)
     {
         return input != Vector2.zero;
     }
 
-    private static Vector2 GetInputVector()
-    {
-        Vector2 input = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            input.x -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            input.x += 1;
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            input.y += 1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            input.y -= 1;
-        }
-
-        return input;
-    }
-
     private Vector3 UpdatePosition(Vector2 input)
     {
-        input = input.normalized;
-
         var moveDir = new Vector3(input.x, 0, input.y);
         transform.position += moveDir * (MOVING_SPEED * Time.deltaTime);
 
