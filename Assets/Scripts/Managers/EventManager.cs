e7ab82054ce5351ac5eadd852838f7d61981af74
Kiet Nguyen <nkiet84@gmail.com>
2023-09-04 15:51:15 +0700

    add plates counter

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -6,5 +6,7 @@ public sealed class EventManager
     public Action CuttingInteract;
     public Action<BaseCounter> SelectCounter;
     public Action<StoveCounter.State> ChangeStoveCounterState;
+    public Action SpawnPlate;
+    public Action RemovePlate;
     public Action<float, int> UpdateCounterProgress;
 }
