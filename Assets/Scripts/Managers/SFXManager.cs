3a82960e66f0e8f989b695a99c71455bc79ec9e4
Kiet Nguyen <nkiet84@gmail.com>
2023-09-20 07:21:37 +0700

    update typo "volumn" to "volume"

--- Assets/Scripts/Managers/SFXManager.cs
+++ Assets/Scripts/Managers/SFXManager.cs
@@ -7,12 +7,12 @@ public sealed class SFXManager : MonoBehaviour
     [SerializeField] private AudioClipRefsSO _audioClipRefsSO;
 
     [Header("Property")]
-    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _chopVolumn;
-    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _pickupVolumn;
-    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _dropVolumn;
-    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _trashVolumn;
-    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _deliverySuccessVolumn;
-    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _deliveryFailedVolumn;
+    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _chopVolume;
+    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _pickupVolume;
+    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _dropVolume;
+    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _trashVolume;
+    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _deliverySuccessVolume;
+    [SerializeField][VTRangeStep(0.0f, 1.0f, 0.1f)] private float _deliveryFailedVolume;
 
     private void OnDestroy()
     {
@@ -41,36 +41,36 @@ public sealed class SFXManager : MonoBehaviour
 
     private void OnCut()
     {
-        PlaySound(_audioClipRefsSO.Chop, Camera.main.transform.position, _chopVolumn);
+        PlaySound(_audioClipRefsSO.Chop, Camera.main.transform.position, _chopVolume);
     }
 
     private void OnPickSomething()
     {
-        PlaySound(_audioClipRefsSO.ObjectPickup, Camera.main.transform.position, _pickupVolumn);
+        PlaySound(_audioClipRefsSO.ObjectPickup, Camera.main.transform.position, _pickupVolume);
     }
 
     private void OnObjectPlaced()
     {
-        PlaySound(_audioClipRefsSO.ObjectDrop, Camera.main.transform.position, _dropVolumn);
+        PlaySound(_audioClipRefsSO.ObjectDrop, Camera.main.transform.position, _dropVolume);
     }
 
     private void OnObjectTrashed()
     {
-        PlaySound(_audioClipRefsSO.Trash, Camera.main.transform.position, _trashVolumn);
+        PlaySound(_audioClipRefsSO.Trash, Camera.main.transform.position, _trashVolume);
     }
 
     private void OnDeliverReceiptSuccess()
     {
-        PlaySound(_audioClipRefsSO.DeliverySuccess, Camera.main.transform.position, _deliverySuccessVolumn);
+        PlaySound(_audioClipRefsSO.DeliverySuccess, Camera.main.transform.position, _deliverySuccessVolume);
     }
 
     private void OnDeliverReceiptFailed()
     {
-        PlaySound(_audioClipRefsSO.DeliveryFail, Camera.main.transform.position, _deliveryFailedVolumn);
+        PlaySound(_audioClipRefsSO.DeliveryFail, Camera.main.transform.position, _deliveryFailedVolume);
     }
 
-    private void PlaySound(AudioClip[] audioClips, Vector3 position, float volumn = 1)
+    private void PlaySound(AudioClip[] audioClips, Vector3 position, float volume = 1)
     {
-        AudioSource.PlayClipAtPoint(audioClips[Random.Range(0, audioClips.Length)], position, volumn);
+        AudioSource.PlayClipAtPoint(audioClips[Random.Range(0, audioClips.Length)], position, volume);
     }
 }
