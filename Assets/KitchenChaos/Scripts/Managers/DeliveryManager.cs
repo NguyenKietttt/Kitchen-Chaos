11c69f747e63888033f6d64e52a96a2a0efebb28
Kiet Nguyen <nkiet84@gmail.com>
2023-11-19 10:44:46 +0700

    [Refactor] Reorganize asset folders

---
+++ Assets/KitchenChaos/Scripts/Managers/DeliveryManager.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class DeliveryManager
+{
+    private const float SPAWN_RECEIPT_TIMER_MAX = 4.0f;
+    private const int WAITING_RECEIPT_MAX = 4;
+
+    public int AmountSucessfulReceipt => _amountSucessfulReceipt;
+
+    private readonly List<ReceiptSO> _waitingListReceiptSO = new();
+    private readonly ListReceiptSO _receiptSOList;
+
+    private float _spawnReceiptTimer;
+    private int _amountSucessfulReceipt;
+
+    public DeliveryManager(ListReceiptSO listReceiptSO)
+    {
+        _receiptSOList = listReceiptSO;
+    }
+
+    public List<ReceiptSO> GetListWaitingReceiptSO()
+    {
+        return _waitingListReceiptSO;
+    }
+
+    public void OnUpdate(float deltaTime)
+    {
+        if (!Bootstrap.Instance.GameStateMgr.IsGamePlaying)
+        {
+            return;
+        }
+
+        _spawnReceiptTimer += deltaTime;
+
+        if (_spawnReceiptTimer >= SPAWN_RECEIPT_TIMER_MAX)
+        {
+            _spawnReceiptTimer = 0;
+
+            if (Bootstrap.Instance.GameStateMgr.IsGamePlaying && _waitingListReceiptSO.Count < WAITING_RECEIPT_MAX)
+            {
+                ReceiptSO waitingReceiptSO = _receiptSOList.ReceiptSOList[Random.Range(0, _receiptSOList.ReceiptSOList.Length)];
+                _waitingListReceiptSO.Add(waitingReceiptSO);
+
+                Bootstrap.Instance.EventMgr.SpawnReceipt?.Invoke();
+            }
+        }
+    }
+
+    public void DeliveryReceipt(PlateKitchenObject plateKitchenObj)
+    {
+        for (int i = 0; i < _waitingListReceiptSO.Count; i++)
+        {
+            ReceiptSO waitingReceiptSO = _waitingListReceiptSO[i];
+            HashSet<KitchenObjectSO> listPlateKichenObjSO = plateKitchenObj.GetListKitchenObjectSO();
+
+            if (waitingReceiptSO.ListKitchenObjSO.Length == listPlateKichenObjSO.Count)
+            {
+                bool isPlateContentMatchesReceipt = true;
+
+                foreach (KitchenObjectSO receiptKitchenObjSO in waitingReceiptSO.ListKitchenObjSO)
+                {
+                    bool isIngredientFound = false;
+
+                    if (listPlateKichenObjSO.Contains(receiptKitchenObjSO))
+                    {
+                        isIngredientFound = true;
+                    }
+
+                    if (!isIngredientFound)
+                    {
+                        isPlateContentMatchesReceipt = false;
+                    }
+                }
+
+                if (isPlateContentMatchesReceipt)
+                {
+                    _amountSucessfulReceipt++;
+                    _waitingListReceiptSO.RemoveAt(i);
+
+                    Bootstrap.Instance.EventMgr.CompleteReceipt?.Invoke();
+                    Bootstrap.Instance.EventMgr.DeliverReceiptSuccess?.Invoke();
+
+                    return;
+                }
+            }
+        }
+
+        Bootstrap.Instance.EventMgr.DeliverReceiptFailed?.Invoke();
+    }
+
+    public void Reset()
+    {
+        _waitingListReceiptSO.Clear();
+
+        _spawnReceiptTimer = 0;
+        _amountSucessfulReceipt = 0;
+    }
+}
