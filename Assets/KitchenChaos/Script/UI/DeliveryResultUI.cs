0a817e48d8cc46401c97f9cbd8a6705a63b21fd8
Kiet Nguyen <nkiet84@gmail.com>
2023-11-29 06:56:40 +0700

    [Refactor] Change delivery counter result animation to DOTween

--- Assets/KitchenChaos/Script/UI/DeliveryResultUI.cs
+++ Assets/KitchenChaos/Script/UI/DeliveryResultUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,16 +10,14 @@ public sealed class DeliveryResultUI : MonoBehaviour
     [SerializeField] private Sprite _failedSprite;
 
     [Header("Internal Ref")]
-    [SerializeField] private Animator _animator;
     [SerializeField] private Image _backgroundImg;
     [SerializeField] private Image _iconImg;
     [SerializeField] private TextMeshProUGUI _messageTxt;
+    [SerializeField] private CanvasGroup _canvasGroup;
 
     [Header("Property")]
     [SerializeField] private Color _successColor;
     [SerializeField] private Color _failedColor;
-
-    private readonly int _popupHashKey = Animator.StringToHash("Popup");
 
     private void Start()
     {
@@ -40,7 +39,7 @@ public sealed class DeliveryResultUI : MonoBehaviour
         _iconImg.sprite = _successSprite;
         _messageTxt.SetText("DELIVERY\nSUCCESS");
 
-        _animator.SetTrigger(_popupHashKey);
+        PlayShowingSequence();
         gameObject.SetActive(true);
     }
 
@@ -50,7 +49,29 @@ public sealed class DeliveryResultUI : MonoBehaviour
         _iconImg.sprite = _failedSprite;
         _messageTxt.SetText("DELIVERY\nFAILED");
 
-        _animator.SetTrigger(_popupHashKey);
+        PlayShowingSequence();
         gameObject.SetActive(true);
+    }
+
+    private void PlayShowingSequence()
+    {
+        // Scale
+        DOTween.Sequence()
+            .AppendCallback(() => transform.localScale = new Vector3(0.6f, 0.6f, 0.6f))
+            .Append(transform.DOScale(new Vector3(1.3f, 1.3f, 1.3f), 0.025f))
+            .Append(transform.DOScale(new Vector3(1.1f, 1.1f, 1.1f), 0.1f))
+            .Append(transform.DOScale(Vector3.one, 1.0f));
+
+        // Rotate
+        DOTween.Sequence()
+            .AppendCallback(() => transform.eulerAngles = new Vector3(0.0f, 0.0f, 15.0f))
+            .Append(transform.DOLocalRotate(new Vector3(0.0f, 0.0f, -15.0f), 0.025f))
+            .Append(transform.DOLocalRotate(Vector3.zero, 0.01f));
+
+        // Fade
+        DOTween.Sequence()
+            .AppendCallback(() => _canvasGroup.alpha = 1.0f)
+            .Append(_canvasGroup.DOFade(1.0f, 0.5f))
+            .Append(_canvasGroup.DOFade(0.0f, 0.5f));
     }
 }
