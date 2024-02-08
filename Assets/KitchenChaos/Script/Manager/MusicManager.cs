ae639b65dd4e7d2486b0c2f29eb287ffc90ae029
Kiet Nguyen <nkiet84@gmail.com>
2024-02-08 15:51:41 +0700

    [Refactor] Convert asset ref fileds & property fields to a SO config in MusicManager file

--- Assets/KitchenChaos/Script/Manager/MusicManager.cs
+++ Assets/KitchenChaos/Script/Manager/MusicManager.cs
@@ -4,38 +4,34 @@ namespace KitchenChaos
 {
     public sealed class MusicManager : MonoBehaviour
     {
-        private const string MUSIC_VOLUMN_KEY = "MUSIC_VOLUMN_KEY";
-
-        private const float MAX_VOLUMN = 1.0f;
-        private const float MIN_VOLUME = 0.0f;
-        private const float DEFAULT_VOLUMN = 0.5f;
-        private const float VOLUME_STEP = 0.1f;
-
         public float MasterVolumn => _masterVolumn;
+
+        [Header("Config")]
+        [SerializeField] private MusicManagerCfg _config;
 
         [Header("Internal Ref")]
         [SerializeField] private AudioSource _audioSrc;
 
-        private float _masterVolumn = DEFAULT_VOLUMN;
+        private float _masterVolumn;
 
         private void Start()
         {
-            _masterVolumn = PlayerPrefs.GetFloat(MUSIC_VOLUMN_KEY, DEFAULT_VOLUMN);
+            _masterVolumn = PlayerPrefs.GetFloat(_config.PlayerPrefsVolumnKey, _config.DefaultVolumn);
             _audioSrc.volume = _masterVolumn;
         }
 
         public void ChangeVolumn()
         {
-            _masterVolumn += VOLUME_STEP;
+            _masterVolumn += _config.VolumnStep;
 
-            if (_masterVolumn > MAX_VOLUMN)
+            if (_masterVolumn > _config.VolumnMax)
             {
-                _masterVolumn = MIN_VOLUME;
+                _masterVolumn = _config.VolumnMin;
             }
 
             _audioSrc.volume = _masterVolumn;
 
-            PlayerPrefs.SetFloat(MUSIC_VOLUMN_KEY, _masterVolumn);
+            PlayerPrefs.SetFloat(_config.PlayerPrefsVolumnKey, _masterVolumn);
             PlayerPrefs.Save();
         }
     }
