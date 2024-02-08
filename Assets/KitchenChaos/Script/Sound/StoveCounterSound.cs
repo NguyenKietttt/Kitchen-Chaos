c8eeb73ccadeda244af88f692e5b0ea0079d6dbc
Kiet Nguyen <nkiet84@gmail.com>
2024-02-08 16:41:28 +0700

    [Refactor] Convert asset ref fileds & property fields to a SO config in StoveCounterSound file

--- Assets/KitchenChaos/Script/Sound/StoveCounterSound.cs
+++ Assets/KitchenChaos/Script/Sound/StoveCounterSound.cs
@@ -4,9 +4,8 @@ namespace KitchenChaos
 {
     public sealed class StoveCounterSound : MonoBehaviour
     {
-        private const float BURN_PROGRESS_AMOUNT = 0.5f;
-        private const float WARNING_SOUND_TIMER_MAX = 0.2f;
-        private const float WARNING_SOUND_TIMER_MIN = 0.0f;
+        [Header("Config")]
+        [SerializeField] private StoveCounterSFXCfg _config;
 
         [Header("External Ref")]
         [SerializeField] private GameObject _stoveCounterObj;
@@ -28,14 +27,14 @@ namespace KitchenChaos
         {
             if (!_shoudPlayWarningSound)
             {
-                _warningSoundTimer = WARNING_SOUND_TIMER_MAX;
+                _warningSoundTimer = _config.WarningTimerMax;
                 return;
             }
 
             _warningSoundTimer -= Time.deltaTime;
-            if (_warningSoundTimer <= WARNING_SOUND_TIMER_MIN)
+            if (_warningSoundTimer <= _config.WarningTimerMin)
             {
-                _warningSoundTimer = WARNING_SOUND_TIMER_MAX;
+                _warningSoundTimer = _config.WarningTimerMax;
                 Bootstrap.Instance.EventMgr.StoveWarning?.Invoke();
             }
         }
@@ -70,7 +69,7 @@ namespace KitchenChaos
                 return;
             }
 
-            _shoudPlayWarningSound = _stoveCounter.IsFried && progressNormalized >= BURN_PROGRESS_AMOUNT;
+            _shoudPlayWarningSound = _stoveCounter.IsFried && progressNormalized >= _config.BurnProgressAmount;
         }
     }
 }
